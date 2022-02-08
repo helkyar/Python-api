db8e3ffc0a7d42803ce30337110945f28faba8fd
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-02-08 15:00:40 +0100

    Add post functionality

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GenreListView
+from .views import GenreListView, GenreCreateView
    
 urlpatterns = [
     path('genre/', GenreListView.as_view(), name='genre'),
+    path('genre/create', GenreCreateView.as_view(), name='genre_create' )
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,16 @@
 # from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView
 
 from .models import Genre
 from .serializers import GenreSerializer
-from rest_framework.generics import ListAPIView
    
+# GET views
 class GenreListView(ListAPIView):
     serializer_class = GenreSerializer
     permission_classes = ()
     queryset = Genre.objects.all()
+
+# POST views
+class GenreCreateView(CreateAPIView):
+    serializer_class = GenreSerializer
+    permission_classes = ()
