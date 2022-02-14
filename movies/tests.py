27c956859a0f4b2027b337c45b34769fc4118333
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-02-14 14:01:17 +0100

    Update test.py

--- movies/tests.py
+++ movies/tests.py
@@ -8,18 +8,18 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
     
     
-class GetBrandsTest(APITestCase):
-""" Test module for GET brands """
+class GetGenresTest(APITestCase):
+""" Test module for GET genres """
     def setUp(self):
-        self.brands = []
-        self.brands.append(baker.make('cars.Brand', name='a'))
-        self.brands.append(baker.make('cars.Brand', name='b'))
-        self.brands_counter = len(self.brands)
-        self.url = reverse('brands')
+        self.genres = []
+        self.genres.append(baker.make('cars.Brand', name='a'))
+        self.genres.append(baker.make('cars.Brand', name='b'))
+        self.genres_counter = len(self.genres)
+        self.url = reverse('genres')
         
-    def test_get_brands_valid(self):
+    def test_get_genres_valid(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['results']), self.brands_counter)
-        self.assertEqual(response.data['results'][0]['id'], self.brands[0].id)
-        self.assertEqual(response.data['results'][1]['id'], self.brands[1].id)
+        self.assertEqual(len(response.data['results']), self.genres_counter)
+        self.assertEqual(response.data['results'][0]['id'], self.genres[0].id)
+        self.assertEqual(response.data['results'][1]['id'], self.genres[1].id)
